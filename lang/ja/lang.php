086049ff2f12efe13a3cc7191b410db6bb2aa562
AlloyDome <77043817+AlloyDome@users.noreply.github.com>
2021-01-07 16:17:40 +0800

    Create lang.php

---
+++ lang/ja/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ */
+$lang['missing_argument']		= '警告：引数を欠きます';
+$lang['selfCallingBegin']		= 'エラー：「';
+$lang['selfCallingEnd']			= '」テンプレートを使用できません。これは、自分自身コールするのが無限再帰を引き起こし、ウェブページが応答しないことが原因です。';
+
+?>
