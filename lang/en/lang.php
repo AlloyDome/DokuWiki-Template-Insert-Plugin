5baace82b97d5f8b81e9d33dacbd3b844dac71cb
AlloyDome <77043817+AlloyDome@users.noreply.github.com>
2021-01-07 16:15:10 +0800

    Delete lang.php

--- lang/en/lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- */
-$lang['missing_argument']		= 'Warning: Argument Missing';
-$lang['selfCallingBegin']		= 'Error: Cannot use template “';
-$lang['selfCallingEnd']			= '” due to its self-calling which will cause infinite recursion, making the webpage no response.';
-
-?>
