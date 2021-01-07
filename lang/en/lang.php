c6cf827b934e704a9857c4eb6b4786c671e01520
AlloyDome <77043817+AlloyDome@users.noreply.github.com>
2021-01-07 16:12:12 +0800

    Create lang.php

---
+++ lang/en/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ */
+$lang['missing_argument']		= 'Warning: Argument Missing';
+$lang['selfCallingBegin']		= 'Error: Cannot use template “';
+$lang['selfCallingEnd']			= '” due to its self-calling which will cause infinite recursion, making the webpage no response.';
+
+?>
