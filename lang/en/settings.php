a316ed8cbafb5a9fdbdf9ce1831485a53c1c6429
AlloyDome <77043817+AlloyDome@users.noreply.github.com>
2021-04-28 21:18:48 +0800

    update settings.php

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -2,6 +2,6 @@
 
 /**
  */
-$lang['namespace']             = 'Default namespace for Wikimedia-style templates';
-
+$lang['namespace']		= 'Default namespace for Wikimedia-style templates';
+$lang['maxNestLevel']	= 'Max level of template nesting ("0" means no limit)';
 ?>
