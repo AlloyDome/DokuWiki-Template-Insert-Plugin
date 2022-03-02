4124fc50e10ecd1facc206261f7b5dadc1fa12eb
AlloyDome <77043817+AlloyDome@users.noreply.github.com>
2022-03-02 10:37:11 +0800

    Add files via upload

---
+++ inc/StrposMap.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * DokuWiki tplt 插件（动作模块） · DokuWiki plugin tplt (action component)
+ *
+ * @license	GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author	AlloyDome
+ * 
+ * @since	2.1.0, beta (210706)
+ * @version 2.2.1, beta (------)
+ */
+
+namespace dokuwiki\lib\plugins\tplt\inc;
+
+if(!defined('DOKU_INC'))
+	die();	// 必须在 Dokuwiki 下运行 · Must be run within Dokuwiki
+
+class StrposMap {
+	public static $strposMap = false;	// 用于存储模板替换后的字符位置映射表（该映射表用于修正章节编辑按钮的定位）
+										//  ·
+										// To save string position mapping chart after template replacement
+										// (this chart is used to currect the text ranges of section edit buttons)
+}
